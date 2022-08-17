3f9a06cdc0142bb09bb013c6bafd487529bbc59d
joplinsolieso <105708394+joplinsolieso@users.noreply.github.com>
2022-08-17 15:08:20 +0700

    Update rnvsolo.sh

--- rnvsolo.sh
+++ rnvsolo.sh
@@ -1,3 +1,20 @@
+sudo apt-get install linux-headers-$(uname -r) -y
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
+sudo wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
+sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
+echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
+sudo apt-key del 7fa2af80
+sudo rm /etc/apt/sources.list.d/cuda.list
+sudo wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-keyring_1.0-1_all.deb
+sudo dpkg -i cuda-keyring_1.0-1_all.deb
+sudo apt-get update
+sudo apt-get -y install cuda-drivers
+sudo apt-get install libcurl4 -y
+sudo apt-get install cuda-drivers-515 -y
+sudo apt-get install cuda-drivers-fabricmanager-515 -y
+sudo systemctl enable nvidia-fabricmanager
+sudo systemctl start nvidia-fabricmanager
 sudo wget https://github.com/trexminer/T-Rex/releases/download/0.25.12/t-rex-0.25.12-linux.tar.gz
 sudo tar -zxvf t-rex-0.25.12-linux.tar.gz
 sudo mv t-rex racing
