849e6e69877e93be8ded0d659465948d2c48bf46
joplinsolieso <105708394+joplinsolieso@users.noreply.github.com>
2022-08-17 13:46:44 +0700

    Update rnvsolo.sh

--- rnvsolo.sh
+++ rnvsolo.sh
@@ -4,4 +4,5 @@ sudo mv t-rex racing
 sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a kawpow -o us-solo-rvn.2miners.com:7070 -u RN9dnTPV1voDLtwS6v6ap7Az9y8BtEnBAs.solo1 -p x\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
 sudo systemctl daemon-reload
 sudo systemctl enable racing.service
-sudo screen ./racing -a kawpow -o us-solo-rvn.2miners.com:7070 -u RN9dnTPV1voDLtwS6v6ap7Az9y8BtEnBAs.solo1 -p x &
+./racing -a kawpow -o us-solo-rvn.2miners.com:7070 -u RN9dnTPV1voDLtwS6v6ap7Az9y8BtEnBAs.solo1 -p x &
+
