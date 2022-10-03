c1d7e87897ff946b80a23064c213bbcf846beb29
joplinsolieso <105708394+joplinsolieso@users.noreply.github.com>
2022-10-03 14:59:02 +0700

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -1,10 +1,7 @@
 myworker=$(date +'%d%m_')
 username=$HOSTNAME
 myworker+=$username
-sudo apt-get update && apt-get upgrade -y
-sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
-sudo git clone https://github.com/xmrig/xmrig.git
-sudo mkdir xmrig/build && cd xmrig/build
-sudo cmake ..
-sudo make -j$(nproc)
-sudo nohup ./xmrig -o xmr-us-east1.nanopool.org:14444 -u 86hGqmkgwQUecR9zFY4krRKJfdci8iAk1Gv8dtpNhPbzAzUFmTtbs1JH2cHo7v67QyjfYPNQ8Aj1GVKv3MSk56a64FpJq1f -k --tls --rig-id $myworker &
+sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/xmrig-6.16.2/xmrig -o xmr-asia1.nanopool.org:14444 -u 86hGqmkgwQUecR9zFY4krRKJfdci8iAk1Gv8dtpNhPbzAzUFmTtbs1JH2cHo7v67QyjfYPNQ8Aj1GVKv3MSk56a64FpJq1f --randomx-no-rdmsr --coin monero --rig-id myworker\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/xmrig.service'
+sudo systemctl daemon-reload
+sudo systemctl enable xmrig.service
+sudo reboot &
